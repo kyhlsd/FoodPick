59ea9d6462af009925f38787d9aad5eb8c869811
kyhlsd <kmyghn@gmail.com>
2025-12-11 18:40:03 +0900

    tuist 모듈 구조 생성

--- Project.swift
+++ Project.swift
@@ -1,34 +1,126 @@
 import ProjectDescription
 
+let iOSVersion = "16.0"
+let teamID = "4QUWH828P3"
+
 let project = Project(
     name: "FoodPick",
     targets: [
         .target(
             name: "FoodPick",
-            destinations: .iOS,
+            destinations: [.iPhone],
             product: .app,
-            bundleId: "io.tuist.FoodPick",
+            bundleId: "com.kyh.FoodPick",
+            deploymentTargets: .iOS(iOSVersion),
             infoPlist: .extendingDefault(
                 with: [
+                    "CFBundleDisplayName": "FoodPick",
                     "UILaunchScreen": [
                         "UIColorName": "",
                         "UIImageName": "",
                     ],
+                    "UIUserInterfaceStyle": "Light",
+                    "UISupportedInterfaceOrientations": [
+                        "UIInterfaceOrientationPortrait"
+                    ],
+                    "NSLocationWhenInUseUsageDescription": "내 주위 가게 정보를 받아오기 위해 위치 정보를 사용합니다."
                 ]
             ),
             sources: ["FoodPick/Sources/**"],
             resources: ["FoodPick/Resources/**"],
-            dependencies: []
+            dependencies: [
+                .target(name: "Presentation"),
+                .target(name: "Domain"),
+                .target(name: "Data")
+            ],
+            settings: .settings(
+                base: [
+                    "DEVELOPMENT_TEAM": .string(teamID)
+                ]
+            )
         ),
-        .target(
-            name: "FoodPickTests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "io.tuist.FoodPickTests",
-            infoPlist: .default,
-            sources: ["FoodPick/Tests/**"],
-            resources: [],
-            dependencies: [.target(name: "FoodPick")]
-        ),
+        
+            .target(
+                name: "FoodPickTests",
+                destinations: .iOS,
+                product: .unitTests,
+                bundleId: "io.tuist.FoodPickTests",
+                infoPlist: .default,
+                sources: ["FoodPick/Tests/**"],
+                resources: [],
+                dependencies: [.target(name: "FoodPick")],
+                settings: .settings(
+                    base: [
+                        "DEVELOPMENT_TEAM": .string(teamID)
+                    ]
+                )
+            ),
+        
+            .target(name: "Core",
+                    destinations: [.iPhone],
+                    product: .framework,
+                    bundleId: "com.kyh.Core",
+                    deploymentTargets: .iOS(iOSVersion),
+                    sources: ["Core/Sources/**"],
+                    dependencies: [],
+                    settings: .settings(
+                        base: [
+                            "DEVELOPMENT_TEAM": .string(teamID)
+                        ]
+                    )
+                   ),
+        
+            .target(name: "Domain",
+                    destinations: [.iPhone],
+                    product: .framework,
+                    bundleId: "com.kyh.Domain",
+                    deploymentTargets: .iOS(iOSVersion),
+                    sources: ["Domain/Sources/**"],
+                    dependencies: [
+                        .target(name: "Core")
+                    ],
+                    settings: .settings(
+                        base: [
+                            "DEVELOPMENT_TEAM": .string(teamID)
+                        ]
+                    )
+                   ),
+        
+            .target(name: "Data",
+                    destinations: [.iPhone],
+                    product: .framework,
+                    bundleId: "com.kyh.Data",
+                    deploymentTargets: .iOS(iOSVersion),
+                    sources: ["Data/Sources/**"],
+                    resources: ["Data/Resources/**"],
+                    dependencies: [
+                        .target(name: "Core"),
+                        .target(name: "Domain")
+                    ],
+                    settings: .settings(
+                        base: [
+                            "DEVELOPMENT_TEAM": .string(teamID)
+                        ]
+                    )
+                   ),
+        
+            .target(name: "Presentation",
+                    destinations: [.iPhone],
+                    product: .framework,
+                    bundleId: "com.kyh.Presentation",
+                    deploymentTargets: .iOS(iOSVersion),
+                    sources: ["Presentation/Sources/**"],
+                    resources: ["Presentation/Resources/**"],
+                    dependencies: [
+                        .target(name: "Domain"),
+                        .target(name: "Data"),
+                        .target(name: "Core")
+                    ],
+                    settings: .settings(
+                        base: [
+                            "DEVELOPMENT_TEAM": .string(teamID)
+                        ]
+                    )
+                   ),
     ]
 )
